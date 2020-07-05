8cfb0c963aab71beaa941531fb7d654aca48e5c6
Nina <62705019+thisAndDead@users.noreply.github.com>
2020-07-05 23:12:52 +0200

    Set test class public

--- src/test/java/at/fhj/iit/GenericQueueAsIntegerTest.java
+++ src/test/java/at/fhj/iit/GenericQueueAsIntegerTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class GenericQueueAsIntegerTest {
+public class GenericQueueAsIntegerTest {
 
 	private GenericsQueue genIntQueue;
 
