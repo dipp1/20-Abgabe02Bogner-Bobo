bd5fe4cda7f76294279ba593f7c797852dbc95ff
last name first name <bognerni@gmail.com>
2020-07-05 14:09:00 +0200

    Set test class to public so mvn site works

--- src/test/java/at/fhj/iit/StringQueueTest.java
+++ src/test/java/at/fhj/iit/StringQueueTest.java
@@ -5,15 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class StringQueueTest {
+public class StringQueueTest {
 
    // TODO Write your own tests
-	
+
 	public StringQueueTest() {
 		super();
 	}
-	
-	
-	
-
 }
